1f6cc9db23b1414986b9b9fd9747675c505abdff
Bhaskar Panja <bhaskarpanja93@gmail.com>
2024-10-20 21:02:40 +0530

    \\ Folder structure changed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import HomePage from './pages/HomePage';
+import Discover from './pages/Discover';
+import Contact from './pages/Contact';
+import Iris from './pages/Iris';
+import Feedback from './pages/Feedback';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Game from './pages/Game';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <Router>
+          <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/discover" element={<HomePage />} />
+              <Route path="/contact" element={<HomePage />} />
+              <Route path="/iris" element={<HomePage />} />
+              <Route path="/feedback" element={<HomePage />} />
+              <Route path="/login" element={<HomePage />} />
+              <Route path="/signup" element={<HomePage />} />
+              <Route path="/game" element={<HomePage />} />
+              <Route path="/game/spacerock" element={<HomePage />} />
+          </Routes>
+      </Router>
+    <HomePage />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/Game.js
@@ -0,0 +1,11 @@
+
+
+function HomePage() {
+  return (
+      <div className="App">
+
+      </div>
+  );
+}
+
+export default HomePage;
