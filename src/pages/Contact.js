b1294fc56e9f3afbaf7e74f55886a3b214afd772
Bhaskar Panja <bhaskarpanja93@gmail.com>
2024-10-24 11:47:59 +0530

    \\ contact works

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,6 @@ import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import Cookies from "js-cookie";
 import checkAuth from "../elements/CheckAuth";
-import removeCSS from "../elements/RemoveCSS";
 import addCSS from "../elements/AddCSS";
 import addJS from "../elements/AddJS";
 
@@ -14,6 +13,7 @@ function Contact() {
         let Bearer = Cookies.get('BEARER');
         checkAuth(Bearer).then(isCorrect => {
             if (isCorrect) {
+                addJS("/js/contact.js");
                 addCSS("/css/contact.css");
                 addCSS("https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
                 addCSS("https://fonts.googleapis.com/css2?family=Poppins&family=Roboto+Slab&display=swap");
@@ -24,11 +24,79 @@ function Contact() {
     }, []);
   let toReturn = (
       <div className="Contact" style={{backgroundColor: "black"}}>
+          <div className="container mt-5">
+              <header>
+                  <h1 style={{color:"azure"}}>Contact Us</h1>
+                  <p style={{color:"azure", fontSize:"larger"}}>We'd excited to hear from you! Whether you have feedback about our games, suggestions for new features, or just want to say hello, feel free to reach out. Your input helps us make our website even better!</p>
+              </header>
 
+              <div className="content">
+                  <div className="content-form">
+                      <section>
+                          <i id="iconFirst" className="fa fa-map-marker fa-2x" aria-hidden="true"></i>
+                          <h2>address</h2>
+                          <p>
+                              Lorem ipsum dolor sit. <br/>
+                              Lorem, ipsum dolor.
 
+                          </p>
+                      </section>
+
+                      <section>
+                          <i className="fa fa-phone fa-2x" aria-hidden="true"></i>
+                          <h2>Phone</h2>
+                          <p>123-456-78901548</p>
+                      </section>
+
+                      <section>
+                          <i className="fa fa-envelope fa-2x" aria-hidden="true"></i>
+                          <h2>E-mail</h2>
+                          <p className="paraThird">dghdgqd@temporary.net</p>
+                      </section>
+                  </div>
+
+                  <form id="contact-form">
+                      <div className="form">
+                          <div className="right">
+                              <div className="contact-form" style={{padding: '1%'}}>
+                                  <span style={{color:"aliceblue"}}>Full Name</span>
+                                  <input type="text" id="name" name="name" required/>
+
+                              </div>
+
+                              <div className="contact-form" style={{padding: '1%'}}>
+                                  <span style={{color:"aliceblue"}}>E-mail Id</span>
+                                  <input type="email" id="email" name="email" required/>
+
+                                  <span className="error-message"></span>
+                              </div>
+
+                              <div className="contact-form" style={{padding: '1%'}}>
+                                  <span style={{color:"aliceblue"}}>Type your Message....</span>
+                                  <input type="text" id="message" name="message" required/>
+
+                              </div>
+
+                              <div className="contact-form" style={{padding: '7%'}}>
+                                  <input type="submit" name="submit" value="Submit" style={{borderRadius: '10px', borderColor:'rgb(111, 108, 153)'}}/>
+                              </div>
+                          </div>
+                      </div>
+                  </form>
+              </div>
+
+              <div className="media">
+                  <ul>
+                      <li><i className="fa fa-facebook-square fa-2x" aria-hidden="true"><a href=""></a></i></li>
+                      <li><i className="fa fa-instagram fa-2x" aria-hidden="true"></i></li>
+                      <li><i className="fa fa-whatsapp fa-2x" aria-hidden="true"></i></li>
+                      <li><i className="fa fa-twitter-square fa-2x" aria-hidden="true"></i></li>
+                  </ul>
+              </div>
+          </div>
       </div>
   );
-  return toRender
+    return toRender
 }
 
 export default Contact;
