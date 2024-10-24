b22fe43426a089da26bb0d1bbdc5a9fa72105177
riyapuri2005 <riyapuri0710@gmail.com>
2024-10-24 10:46:16 +0530

    added game.js and changed location of angryman

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,11 +1,10 @@
-
-
 function Game() {
   return (
       <div className="Game">
-
+        <iframe src="https://www.example.com" title="Embedded Page"></iframe>
       </div>
-  );
+)
+    ;
 }
 
 export default Game;
