6246e3ba8982fb1145e7fcfb6e156ced1a8cb855
Bhaskar Panja <bhaskarpanja93@gmail.com>
2024-11-11 00:07:54 +0530

    \\

--- src/common_elements/AddCSS.js
+++ src/common_elements/AddCSS.js
@@ -1,9 +1,12 @@
 function addCSS(URL){
-    const linkObj = document.createElement('link');
-    linkObj.rel = "stylesheet";
-    linkObj.href = URL;
     const existingLinkObj = document.querySelector("link[href='"+URL+"']");
-    if (!existingLinkObj) { document.head.appendChild(linkObj); console.log("Added CSS", URL); }
+    if (!existingLinkObj)
+    {
+        const linkObj = document.createElement('link');
+        linkObj.rel = "stylesheet";
+        linkObj.href = URL;
+        document.head.appendChild(linkObj); console.log("Added CSS", URL);
+    }
 }
 
 export default addCSS;
--- src/common_elements/AddJS.js
+++ src/common_elements/AddJS.js
@@ -1,8 +1,11 @@
 function addJS(URL){
-    const scriptObj = document.createElement('script')
-    scriptObj.src = URL
     const existingScriptObj = document.querySelector("script[src='"+URL+"']")
-    if (!existingScriptObj) { document.head.appendChild(scriptObj); console.log("Added JS ", URL); }
+    if (!existingScriptObj)
+    {
+        const scriptObj = document.createElement('script')
+        scriptObj.src = URL
+        document.head.appendChild(scriptObj); console.log("Added JS ", URL);
+    }
 }
 
 export default addJS;
